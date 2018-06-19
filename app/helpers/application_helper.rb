4f6dd04b0677d523886a24027007966a953a2b5d
Ramon <ramonlrodriguez@gmail.com>
2018-06-19 10:30:57 -0500

    update markdown rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,23 @@ module ApplicationHelper
   # https://richonrails.com/articles/rendering-markdown-with-redcarpet
   def markdown(text)
     options = {
-      filter_html:     true,
-      hard_wrap:       true,
-      link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true,
-      fenced_code_blocks: true
+      filter_html:                  false,
+      hard_wrap:                    true,
+      link_attributes:              { rel: 'nofollow',
+                                      target: "_blank" },
+      prettify:                     true
     }
 
     extensions = {
-      autolink:           true,
-      superscript:        true,
-      disable_indented_code_blocks: true
+      autolink:                     true,
+      disable_indented_code_blocks: true,
+      fenced_code_blocks:           true,
+      highlight:                    true,
+      no_intra_emphasis:            true,
+      quote:                        true,
+      strikethrough:                true,
+      superscript:                  true,
+      tables:                       true
     }
 
     renderer = Redcarpet::Render::HTML.new(options)
